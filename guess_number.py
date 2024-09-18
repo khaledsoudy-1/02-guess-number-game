32a7acc8014d1297d705a8c34f2b201c06a648bc
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-19 01:55:47 +0300

    Add play-again loop and input-validation.

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,32 @@
 import random
 
-# Generate a random number between 1 and 3 (inclusive)
-computer_number = random.randint(1, 3)
-
-# Ask the player to guess the number
-player_number = int(input("Guess which number I'm thinking of .. 1, 2 or 3:\n"))
-
-# Check if the player's guess matches the computer's number
-if computer_number == player_number:
-    print("🎉🎉 You win!")
-else:
-    print("😢 You Lose!")
+while True:
+    # Generate a random number between 1 and 3 (inclusive)
+    computer_number = random.randint(1, 3)
+    
+    # Ask the player to guess the number
+    player_number = int(input("Guess which number I'm thinking of .. 1, 2 or 3:\n"))
+    
+    # Check if the player's guess matches the computer's number
+    if computer_number == player_number:
+        print("\n🎉🎉 You win!")
+    else:
+        print("\n😢 You Lose!")
+    
+    # Ask user if they want to play again.
+    print("\nPlay again?!")
+    playagain = input("Y for Yes\nQ to Quit\n").strip().lower()
+    
+    # Validate input.
+    while playagain not in ["y", "q"]:
+        playagain = input("Y for Yes\nQ to Quit\n").strip().lower()
+        
+    # Determine the winner.
+    if playagain == "y":
+        print("==== Welcome Back 😍 ====")
+        continue
+    else:
+        print("\n🎉🎉🎉🎉\nThank you for playing\nBye 👋")
+        break
+        
+        
