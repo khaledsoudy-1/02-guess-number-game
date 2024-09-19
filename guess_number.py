9b66d1f2a5ecf237efb6edae051422151851d56f
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-19 17:52:07 +0300

    Make the comments more readable.

--- guess_number.py
+++ guess_number.py
@@ -2,7 +2,7 @@ import random
 
 
 def guess_number():
-    # Add some counters for a better game.
+    # Initialize game counters
     game_counter = 0
     player_wins = 0
     computer_wins = 0
@@ -12,22 +12,22 @@ def guess_number():
         computer_number = random.randint(1, 3)
         
         try:
-            # Ask the player to guess the number
+            # Get player's guess
             player_number = int(input("Guess which number I'm thinking of .. 1, 2 or 3:\n"))
             
             # Validate player input.
             if player_number not in [1, 2, 3]:
                 print("Please enter a number between 1 and 3.\n")
                 continue
-                
+            
             # Display player and computer numbers.
             print(f"\nYou chose: {player_number}\nComputer chose: {computer_number}\n")
-        
+            
             def decide_winner(player, computer):
                 nonlocal game_counter
                 nonlocal player_wins
                 nonlocal computer_wins
-               
+                
                 # Check if the player's guess matches the computer's number
                 if player == computer:
                     player_wins += 1
@@ -35,7 +35,7 @@ def guess_number():
                 else:
                     computer_wins += 1
                     return f"Sorry ... better luck next time. 😢"
-        
+            
             game_result = decide_winner(player_number, computer_number)
             print(game_result)
             game_counter += 1
@@ -43,17 +43,17 @@ def guess_number():
         except ValueError:
             print("Invalid input!! Please enter a number.\n")
             continue
-    
-        # Display The counters
+        
+        # Display game statistics
         print(f"\nGame Count: {game_counter}")
         print(f"Your wins: {player_wins}")
         print(f"Computer Wins: {computer_wins}")
-    
+        
         # Ask user if they want to play again.
         print("\nPlay again?!")
         playagain = input("Y for Yes\nQ to Quit\n").strip().lower()
         
-        # Validate input.
+        # Validate play again input.
         while playagain not in ["y", "q"]:
             playagain = input("Y for Yes\nQ to Quit\n").strip().lower()
         
